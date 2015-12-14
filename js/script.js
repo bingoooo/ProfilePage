f4233eb7f38d9bb723134e8f61fcd203ff830449
Dant Benjamin <sukkhato@gmail.com>
2015-12-14 16:44:53 +0100

    -M à J Implémentation du fichier JS site One page relativement fonctionnel

--- js/script.js
+++ js/script.js
@@ -1,8 +1,34 @@
 console.log('Linked : script.js');
 
-$.each(['a', 'b', 'c'], function(index, item){
-  console.log('item %d is: %s', index, item)
+$(document).ready(function(){
+	console.log('Linked : zepto');
+	$('.text-info').hide();
+	$('.default').show();
+
+	$('#personal-data').on('click', function(){
+		$('.text-info').hide();
+		$('#personal-data-content').show();
+		$('#current-page').html('Données Personnelles');
+	});
+	$('#cv').on('click', function(){
+		$('.text-info').hide();
+		$('#cv-content').show();
+		$('#current-page').html('Cursus Scolaire');
+	});
+	$('#pro').on('click', function(){
+		$('.text-info').hide();
+		$('#pro-content').show();
+		$('#current-page').html('Compétences Professionnelles');
+	});
+	$('#other').on('click', function(){
+		$('.text-info').hide();
+		$('#other-content').show();
+		$('#current-page').html('Autres');
+	})
 });
+/*$.each(['a', 'b', 'c'], function(index, item){
+  console.log('item %d is: %s', index, item)
+});*/
 
 /*$(document).ready(function(){
 	console.log('Linked : jquery.js');
