f76e66dc035a3060515b89c91ae341c70b9308ad
Dant Benjamin <sukkhato@gmail.com>
2015-12-14 14:36:14 +0100

    -Version HTML/CSS

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ProfilePage
 Page de Profil
+
+	Installation de zepto via npm
+-npm init (dans le cas où npm installerait les modules dans un autre dossier)
+-npm install zepto
+ou
+-npm install zepto --save
--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 console.log('Linked : script.js');
 
-$(document).ready(function(){
-	console.log('Linked : jquery.js');
+$.each(['a', 'b', 'c'], function(index, item){
+  console.log('item %d is: %s', index, item)
 });
+
+/*$(document).ready(function(){
+	console.log('Linked : jquery.js');
+});*/
