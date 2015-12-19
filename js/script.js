fa0924c6a1af3e07977ee6ffbd1e5996d005db37
Dant Benjamin <sukkhato@gmail.com>
2015-12-20 00:28:28 +0100

    -Version 1.0.4 Ajout d'une fontion de rotation des users

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
+"use strict";
 var personalData;
 var defaultUrl = "https://s.idsympa.com/u-23.json";
 var user = window.location.search.substring(1);
+var currentPosition;
 var usersId = ["1","2","3","4","5","06","7","8","9","10","11","12","13","14","15","16","17","19","20","21","22","23","24"];
 var userUrl = defaultUrl;
 for (var i = 0; i < usersId.length; i++){
 	if (usersId[i] == user){
 		userUrl = "https://s.idsympa.com/u-" + user + ".json";
+		currentPosition = i;
 	}
 };
+
 
 $(document).ready(function(){
 	function ajaxCall(){
@@ -23,13 +27,9 @@ $(document).ready(function(){
 				var userHead = "<h1>{{first_name}} {{last_name}}</h1><h2>{{occupation}}</h2>";
 				var html2 = Mustache.to_html(userHead, json);
 				$('#head-template').html(html2);
-				console.log("Ajax transfert complete");
 			},
 			error : function(){
 				console.log('Erreur de récupération ajax');
-			},
-			complete : function(){
-				console.log('Requête ajax complete');
 			}
 		});
 	}
@@ -46,6 +46,8 @@ $(document).ready(function(){
 			$('#pro').on('click', function(event){app.disp('#data-content', 'ajax/pro.html', 'Compétences Professionnelles');});
 			$('#contact').on('click', function(event){app.popup()});
 			$('.overlay').on('click', function(event){app.popup()});
+			$('#previous').on('click', function(){app.previousUser()});
+			$('#next').on('click', function(){app.nextUser()});
 		},
 		disp : function(target, file, data){
 			$(target).load(file);
@@ -54,6 +56,22 @@ $(document).ready(function(){
 		},
 		popup : function(){
 			$('.overlay').toggle();
+		},
+		nextUser : function(){
+			currentPosition++;
+			if (currentPosition >= usersId.length){
+				currentPosition = 0;
+			}
+			userUrl = "https://s.idsympa.com/u-" + usersId[currentPosition] + ".json";
+			ajaxCall();
+		},
+		previousUser : function(){
+			currentPosition--;
+			if (currentPosition < 0){
+				currentPosition = 23;
+			}
+			userUrl = "https://s.idsympa.com/u-" + usersId[currentPosition] + ".json";
+			ajaxCall();
 		}
 	};
 	app.init();
